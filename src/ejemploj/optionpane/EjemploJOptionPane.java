2273cf935d99162f5a5b85f1bc21588875106e53
Alumno <Alumno@328-05>
2017-04-07 14:55:30 -0500

    Message Dialog

---
+++ src/ejemploj/optionpane/EjemploJOptionPane.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploj.optionpane;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Alumno
+ */
+public class EjemploJOptionPane {
+
+   //Ejemplo de Message Dialog
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Esta es una ventana de tipo Mostrar dialogo. ¿Entendido?", "Ventana de texto", JOptionPane.WARNING_MESSAGE );
+        
+        
+    }
+    
+}
